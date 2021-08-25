5db5e9479f0be1b677c3a53fa5faa3afd677ba19
Blaine <36714770+BlaineGH@users.noreply.github.com>
2021-08-25 16:17:52 -0500

    Add project files.

---
+++ PlayingWithDjango/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Blain\OneDrive\Documents\Programming\Web\PythonProjects\Django\PlayingWithDjango\PlayingWithDjango\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
